defbfa6f3d4f809fcfc986880b1a6c667f563462
Arifin79 <arifin.nurhidayah1933@gmail.com>
2023-11-05 21:34:57 +0700

    update py to php

---
+++ resep.php
@@ -0,0 +1,11 @@
+<?php
+echo "Resep Krabby Patty";
+echo "------------------";
+echo "1. Bawang";
+echo "2. Ketimun";
+echo "3. Roti";
+echo "4. Keju";
+echo "5. Tomat";
+?>
+
+
--- resep.py
+++
@@ -1,7 +0,0 @@
-print("Resep Krabby Patty")
-print("------------------")
-print("1. Bawang")
-print("2. Ketimun")
-print("3. Roti")
-print("4. Keju")
-print("5. Tomat")
